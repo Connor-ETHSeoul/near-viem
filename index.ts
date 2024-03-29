8f03a706ca766c2fb1d927ec525efb9c0874384f
Yoonhyoung-Lee <54613623+Yoonhyoung-Lee@users.noreply.github.com>
2024-03-29 22:24:23 +0900

    initial copy from pkp-viem

---
+++ index.ts
@@ -0,0 +1,3 @@
+import { PKPViemAccount } from "./lib/pkp-viem";
+import { convertAccountToSmartAccountSigner } from "./lib/utils/accountToSmartAccount";
+export { PKPViemAccount, convertAccountToSmartAccountSigner };
